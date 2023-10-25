1b363a04adcaa8b10a2a28efa517ab22bd48cb6d
Jackil-Rajnicant <43136761+Jackil-Rajnicant@users.noreply.github.com>
2023-10-25 11:06:51 +0100

    removed lmk config

--- src/modules/utils/db-connect.ts
+++ src/modules/utils/db-connect.ts
@@ -3,19 +3,6 @@ const odbc = require('odbc');
 let connection: any;
 
 async function connectToDatabase() {
-    // const connection1 = await odbc.connect('DSN=MYDSN');
-
-    // const connectionConfig = {
-    //     connectionString: 'driver=Ingres;server=newlandmarkiitest.itvplc.ads;port=ET7;database=ITVDEVKR/remote;uid=atskruat;pwd=$0naRst@k',
-    //     connectionTimeout: 10,
-    //     loginTimeout: 10,
-    // }
-    // connection = await odbc.connect('DSN=Ingres');
-
-    // connection = await odbc.connect('DSN=IngresDSN');
-    // connection1 is now an open Connection
-
-    // or using a configuration object
     const connectionConfig = { 
         connectionString: 'DSN=IngresDSN',
         connectionTimeout: 10,
